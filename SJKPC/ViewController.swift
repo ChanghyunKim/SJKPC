cf84a26b8af3c541473e2e21fbed568ada97da83
Changhyun Kim <kbspkpk@naver.com>
2020-07-19 17:09:36 -0700

    Initial Commit

---
+++ SJKPC/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SJKPC
+//
+//  Created by 김창현 on 7/19/20.
+//  Copyright © 2020 ChanghyunKim. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
